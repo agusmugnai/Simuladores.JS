7a31c170f81374a26ed856881539d04984a4987d
Agustina Mugnai <agusmugnai@gmail.com>
2023-03-02 17:11:50 -0300

    cambios en el main

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 
-const masaMadre = 0.20;
-let cantHarina = parseInt(prompt("¿Cuánta harina va a utilizar? (Coloque la cantidad en gramos)"));
+const porcentMasaMadre = 0.20;
+let cantHarina = parseInt(prompt(`¿Cuánta harina va a utilizar? (Coloque la cantidad en gramos)`));
 
-function masa(valorUno, valorDos){
-return cantHarina * masaMadre;
+function masa(valorUno, valorDos) {
+  return cantHarina * porcentMasaMadre;
 }
 
-let resultado = masa(cantHarina, masaMadre);
-console.log("Necesita usar " + resultado + "gr de masa madre para su pan.");
+let resultado = masa(cantHarina, porcentMasaMadre);
+alert(`Necesita ${resultado}gr de masa madre para su pan.`);
 
 //CICLO
 
-const respCorrecta = "2023";
+const respCorrecta = `2022`;
 
-for (let i = 0; i <= 3; i++){
-  let preguntaUno = prompt("¿En que año ganó Argentina la tercer copa del mundo en fútbol?");  
-  if(preguntaUno === respCorrecta){
-    alert("Felicitaciones. ¡Su respuesta correcta!");
-    break;   
+for (let i = 1; i <= 3; i++) {
+  let preguntaUno = prompt(`¿En que año ganó Argentina la tercer copa del mundo en fútbol?`);
+  if (preguntaUno === respCorrecta) {
+    alert(`Felicitaciones. ¡Su respuesta correcta!`);
+    break;
   } else {
-    alert("Respuesta incorrecta. Intente nuevamente.");
+    alert(`Respuesta incorrecta. Intente nuevamente.`);
   }
 }
