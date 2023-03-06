9ae8fc1d10128993e8eedec22ac9c3a70442f2bb
Agustina Mugnai <agusmugnai@gmail.com>
2023-03-06 15:16:09 -0300

    nueva modificación en proyecto

--- main.js
+++ main.js
@@ -10,34 +10,32 @@ function plazoFijo(valorUno, valorDos, valorTres) {
   return resultado;
 }
 
-let mensual = plazoFijo(montoDinero, rendimientoAnual, 12);
-let bimestral = plazoFijo(montoDinero, rendimientoAnual, 6);
-let trimestral = plazoFijo(montoDinero, rendimientoAnual, 4);
-let cuatrimestral = plazoFijo(montoDinero, rendimientoAnual, 3);
-let bianual = plazoFijo(montoDinero, rendimientoAnual, 2);
-let anual = plazoFijo(montoDinero, rendimientoAnual, 1);
 
+for (let i = 0; i < 1; i++) {
+  let mensual = plazoFijo(montoDinero, rendimientoAnual, 12);
+  let bimestral = plazoFijo(montoDinero, rendimientoAnual, 6);
+  let trimestral = plazoFijo(montoDinero, rendimientoAnual, 4);
+  let cuatrimestral = plazoFijo(montoDinero, rendimientoAnual, 3);
+  let bianual = plazoFijo(montoDinero, rendimientoAnual, 2);
+  let anual = plazoFijo(montoDinero, rendimientoAnual, 1);
 
-switch (opcionTiempo) {
-  case (1):
-    alert(`Su interés mensual será de $${mensual}`);
-    break;
-  case (2):
-    alert(`Su interés bimestral será de $${bimestral}`);
-    break;
-  case (3):
-    alert(`Su interés trimestral será de $${trimestral}`);
-    break;
-  case (4):
-    alert(`Su interés cuatrimestral será de $${cuatrimestral}`);
-    break;
-  case (5):
-    alert(`Su interés bianual será de $${bianual}`);
-    break;
-  case (6):
-    alert(`Su interés anual será de $${anual}`);
-    break;
-  default:
-    alert("Por favor ingrese una opción válida:\n1) 30 días\n2) 60 días\n3) 90 días\n4) 120 días\n5) 180 días\n6) Anual");
+  if (opcionTiempo == 1) {
+    alert(`Su rendimiento mensual será de $${mensual}.`);
+  } else if (opcionTiempo == 2) {
+    alert(`Su rendimiento bimestral será de $${bimestral}.`);
+  } else if (opcionTiempo == 3) {
+    alert(`Su rendimiento trimestral será de $${trimestral}.`);
+  } else if (opcionTiempo == 4) {
+    alert(`Su rendimiento cuatrimestral será de $${cuatrimestral}.`);
+  } else if (opcionTiempo == 5) {
+    alert(`Su rendimiento bianual será de $${bianual}.`);
+  } else if (opcionTiempo == 6) {
+    alert(`Su rendimiento anual será de $${anual}.`);
+  } 
+  else {
+    alert("Opción no válida. Por favor, actualice la página y elija una nueva opción.");
+  }
 }
+
+
 
